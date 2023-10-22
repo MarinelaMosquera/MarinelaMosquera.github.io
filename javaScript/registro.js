d2808ffbc975eef0794d6f5d353deb2b072cbb73
MarinelaMosquera <139980561+MarinelaMosquera@users.noreply.github.com>
2023-10-22 11:25:57 -0500

    se agrego la parte del script, la function enviar

--- javaScript/registro.js
+++
@@ -1,9 +0,0 @@
-function enviar(){
-    const Nombre = document.getElementById('Nombre').value
-    const Apellidos = document.getElementById('Apellidos').value
-    const Correo = document.getElementById('Correo electrónico').value
-    const gender = document.getElementById('gender').value
-    const password = document.getElementById('Contraseña').value
-
-    console.log(Nombre,Apellidos,Correo,gender,password);
-}
