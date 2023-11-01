6fae9d46b46973ad289b23d0c1aa1a4133d0568f
MarinelaMosquera <139980561+MarinelaMosquera@users.noreply.github.com>
2023-11-01 13:49:31 -0500

    Se creo esta sección

--- javaScript/registro.js
+++ javaScript/registro.js
@@ -1,2 +1,37 @@
-localStorage.setItem('nombre', registrarse);
-var cat = localStorage.getItem("nombre");
+var Registrarse = [];
+
+
+
+function addRegistrarse(Nombre,Apellido,Email,Contraseña,Gender){
+    
+    var nuevoUsuario = {
+        Nombre : Nombre,
+        Apellido : Apellido,
+        Email : Email,
+        Password : Contraseña,
+        Gender : GenderSelect
+        
+    };
+
+    console.log(nuevoUsuario);
+    Registrarse.push(nuevoUsuario);
+    localStoreRegistrarse(Registrarse);
+
+}
+
+function getRegistrarse(){
+    var storeRegistro = localStorage.getItem('localRegistrarse');
+    if(storeRegistro == null){
+        Registrarse =[];
+    }else{
+        Registrarse = JSON.parse(storeRegistro);
+    }
+
+    return Registrarse;
+
+}
+
+function localStoreRegistrarse(Registro){
+    localStorage.setItem('localRegistrarse', JSON.stringify(Registro));
+}
+
