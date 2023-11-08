0f592f63f7a4825d573358ddf8c02d45b501c707
MarinelaMosquera <139980561+MarinelaMosquera@users.noreply.github.com>
2023-11-08 16:29:54 -0500

    Se agregaron ejecución para del footer y el inicio

---
+++ javaScript/footer.js
@@ -0,0 +1,38 @@
+const createfooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML= `
+    <div class="contenido-footer">
+    <img src="imagenes.jpg/logo-2.jpg" class="logo" alt="">
+    <div class="footer-container">
+        <ul class="categoria">
+            <li class="categoria-titulo">Ropa</li>
+            <li><a href="#" class="footer-link">Blusas y Croptops</a></li>
+            <li><a href="#" class="footer-link">Pantalones</a></li>
+            <li><a href="#" class="footer-link">Faldas</a></li>
+            <li><a href="#" class="footer-link">Vestidos</a></li>
+        </ul>
+    </div>
+</div>
+<p class="footer-titulo">Acerca de la compañia</p>
+<p class="informacion"> Somos una empresa dedicada al diseño, producción y comercialización
+de propuestas completas de vestuario y accesorios para mujeres. Creamos, innovamos y 
+sorprendemos con todo lo que hacemos.</p>
+
+<p class="informacion">Soporte Emails-<a href="mailto: proyectocuenta929@gmail.com">proyectocuenta929@gmail.com</a></p>
+<p class="informacion">Telefono- <a href="tel: 3023071678">3023071678</a></p>
+<div class="footer-social">
+    <div>
+        <a href="#" class="social-link">Términos y condiciones</a>
+        <a href="#" class="social-link">Privacidad</a>
+    </div>
+    <div>
+        <a href="#" class="social-link">instagram</a>
+        <a href="#" class="social-link">Facebook</a>
+    </div>
+</div>
+<p class="creditos-footer">Corita shop, tienda online</p>
+    `;
+}
+
+createfooter();
